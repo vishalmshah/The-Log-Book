55e069b24ecd1842f57de69a9e761370f58c01c7
Vishal Shah <vishal@Vishbook.local>
2026-05-16 23:00:09 -0400

    removed pages dir

--- pages/dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# TODO: Load in all data from the database
-# Plot info on weekly focus areas, and session focuses
-# Also on exercise frequency and time spent practicing
-
-# Dashboard content
-st.markdown("# Dashboard")
-st.sidebar.markdown("# Dashboard")
